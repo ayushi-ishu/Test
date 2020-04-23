49aee887d49b61365df2fc61ac097db602c0a72b
Ayushi <ayushirakwal20@gmail.com>
2020-04-23 13:07:53 +0530

    added hello prompt

--- hello.php
+++ hello.php
@@ -5,7 +5,15 @@
 </head>
 
 <body>
-The content of the document...!!!
+This is the content of the document...!!!
+<p>
+WordPress shortcodes are used to;
+1) Reduce the amount of code you need to write,
+2) Simplify the usage of WordPress plugins,
+   themes, and other functions. 
+3) They behave like macros, when you insert a 
+   shortcode, it is replaced with a snippet of code. It could be anything.
+</p>
 </body>
 
 </html>
